b699b047bfcbe9ca13c3b7e3065ec92ac1d1a715
zoe <jesuiszoekim@gmai.com>
2021-01-28 15:47:36 +0900

    signupmodel

--- user/views.py
+++ user/views.py
@@ -2,28 +2,43 @@ from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 from .models import User
 from django.contrib import auth
+from django.contrib.auth.models import User
+
 
 
 def home(request):
-    return render(request,"user/home.html")
+    return render(request,"home.html")
 
 def signup(request):
-    if request.method == 'POST':
-        # User has info and wants an account now!
+    if request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.get(username=request.POST['username'])
-                return render(request, 'user/signup.html', {'error': 'Username has already been taken'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(
-                    request.POST['username'], password=request.POST['password1'])
-                auth.login(request, user)
-                return redirect('user/home.html')
-        else:
-            return render(request, 'user/signup.html', {'error': 'Passwords must match'})
-    else:
-        # User wants to enter info
-        return render(request, 'user/signup.html')
-    if not ( userid and username and password and gender):
-   data['error'] = '모든 값을 입력해주세요.'
+            user = User.objects.create_user(
+                username=request.POST['username'], password=request.POST['password1']
+            )
+
+        signup = Signup()
+        signup.name = request.POST['name']
+        signup.user = user
+        signup.save()
+        auth.login(request, user)
+        return redirect('home')
+    return render(request, 'signup.html')
+
+
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
