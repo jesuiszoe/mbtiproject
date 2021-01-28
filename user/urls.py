714d23ff03f8df774746ab8786b99e4bd22d3454
zoe <jesuiszoekim@gmai.com>
2021-01-28 22:59:33 +0900

    create기능구현

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,16 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
+from .models import Post
 
 urlpatterns = [
         path('', views.home, name='home'),
         path('signup/', views.signup, name='signup'),
         path('login/', views.login, name='login'),
         path('logout/', views.logout, name='logout'),
-        path('detail/', views.detail, name='detail'),
-
+        path('detail/<int:id>', views.detail, name='detail'),
+        path('post/', views.show_post, name='post'),
+        path('new/', views.new, name = 'new'),
+        path('new/create', views.create, name='create'),
 
         ]
